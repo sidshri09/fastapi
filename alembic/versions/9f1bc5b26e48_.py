2a57417afcd0787675b59d90466e8345add884f1
sidshri <sidshri@gmail.com>
2022-01-19 18:28:24 +0530

    setting down_revision to None

--- alembic/versions/9f1bc5b26e48_.py
+++ alembic/versions/9f1bc5b26e48_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '9f1bc5b26e48'
-down_revision = '53225c7ccf20'
+down_revision = None
 branch_labels = None
 depends_on = None
 
